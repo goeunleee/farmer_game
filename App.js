308b95e57d4de25476f067333c01552d48198245
goeun lee <goeun3501@gmail.com>
2024-09-30 15:13:58 +0900

    [feat/css]asset분리, 배경 추가

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-web";
+import FarmerIcon from "./component/farmer.svg";
 import FarmerGame from "./component/game";
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>farmer game!</Text>
+			<FarmerIcon width={100} height={100} />
+			<Text>농부 게임</Text>
+			<Text>화면위의 당근을 손으로 쓸어보세요</Text>
 			{/* <Button>버튼</Button> */}
 			{/* 웹뷰에서는안보임 */}
-			<StatusBar style="auto" />
+			{/* <StatusBar style="auto" /> */}
 			{/* 웹뷰에서는안보임 */}
 			<FarmerGame />
 		</View>
@@ -22,6 +25,6 @@ const styles = StyleSheet.create({
 		backgroundColor: "#fff",
 		alignItems: "center",
 		justifyContent: "center",
-		marginTop: 50,
+		marginTop: 10,
 	},
 });
