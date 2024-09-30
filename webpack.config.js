62db2765961aa7f4f8194849e6d89af4d872e904
goeun lee <goeun3501@gmail.com>
2024-09-30 17:14:00 +0900

    [deploy]expo webpack 빌드 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const { withExpo } = require("@expo/webpack-config");
+const createExpoWebpackConfigAsync = require("@expo/webpack-config");
 
 module.exports = async function (env, argv) {
-	const config = await withExpo(env, argv);
+	const config = await createExpoWebpackConfigAsync(env, argv);
 
 	return config;
 };
